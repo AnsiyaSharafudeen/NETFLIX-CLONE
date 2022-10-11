136f6da0d09b007c21f8e747e0a5106baf5f996a
AnsiyaSharafudeen <ansiyas1997@gmail.com>
2022-10-11 18:54:52 +0400

    final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import React from "react";
 import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import {originals,action,comedyMovies,documentaries,horrorMovies,romanceMovies} from "./urls"
+
+
 
 
 function App() {
@@ -11,6 +15,13 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comedyMovies} title='Comedy' isSmall/>
+      <RowPost url={horrorMovies} title='Horror' isSmall/>
+      <RowPost url={romanceMovies} title='Romance' isSmall/>
+      <RowPost url={documentaries} title='Documentaries' isSmall/>
+
 
     </div>
   );
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3/'
+export const API_KEY="2d7f9aafde37821866c9bf1bdbc0b0c7"
+export const imageUrl="https://image.tmdb.org/t/p/original"
